42a20515c90f588fe3ed3aada4c9f2395b34903d
Ichith Ramanna <ichith.ramanna@gmail.com>
2026-01-13 04:29:21 +0100

    Remove variable.tf after renaming to variables.tf for clean commits

--- variable.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  description = "AWS region to deploy resources"
-  type        = string
-}
-
-variable "aws_profile" {
-  description = "AWS CLI profile name to use"
-  type        = string
-}
