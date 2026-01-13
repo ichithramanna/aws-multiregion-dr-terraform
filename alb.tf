731c51d33d4ea6f657a33da640e422e16131f569
Ichith Ramanna <ichith.ramanna@gmail.com>
2026-01-13 21:12:30 +0100

    Add application load balancer, target group, and listener

---
+++ alb.tf
@@ -0,0 +1,47 @@
+resource "aws_lb" "app_alb" {
+  name               = "three-tier-alb"
+  load_balancer_type = "application"
+  internal           = false
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [
+    aws_subnet.public_a.id,
+    aws_subnet.public_b.id
+  ]
+
+  tags = {
+    Name = "three-tier-alb"
+  }
+}
+
+#create target group
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.three_tier_vpc.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "app-target-group"
+  }
+}
+
+# create alb listener
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
