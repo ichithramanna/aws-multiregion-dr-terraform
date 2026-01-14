ecdc2aa47f2cabccd0e4b89de508c46036bc0a7b
Ichith Ramanna <ichith.ramanna@gmail.com>
2026-01-14 01:58:03 +0100

    Add frontend infrastructure with S3, CloudFront

---
+++ frontend_s3.tf
@@ -0,0 +1,52 @@
+#creating S3 bucket
+resource "aws_s3_bucket" "frontend" {
+  bucket = "my-frontend-bucket-tta-10-25"
+}
+
+#Block all public access for cloudfront-only access
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+#Upload a static HTML file
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.frontend.id
+  key          = "index.html"
+  content_type = "text/html"
+
+  content = <<EOF
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Three Tier App</title>
+</head>
+<body>
+  <h1>Frontend served from S3 + CloudFront</h1>
+</body>
+</html>
+EOF
+}
+
+#adding policy for Only this CloudFront distribution read
+resource "aws_s3_bucket_policy" "frontend" {
+  
+  bucket = aws_s3_bucket.frontend.id
+  policy = jsonencode({
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "cloudfront.amazonaws.com" }
+      Action   = "s3:GetObject"
+      Resource = "${aws_s3_bucket.frontend.arn}/*"
+      Condition = {
+        StringEquals = {
+          "AWS:SourceArn" = aws_cloudfront_distribution.frontend.arn
+        }
+      }
+    }]
+  })
+}
