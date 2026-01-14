b9a435ee0704d8bcd03c87291f62763eb1ab172c
Ichith Ramanna <ichith.ramanna@gmail.com>
2026-01-14 08:11:19 +0100

    removed the variable aws_profile

--- variables.tf
+++ variables.tf
@@ -2,8 +2,3 @@ variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
 }
-
-variable "aws_profile" {
-  description = "AWS CLI profile name to use"
-  type        = string
-}
